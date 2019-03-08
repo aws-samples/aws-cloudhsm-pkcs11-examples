153d3498aa0d031d3bf9532e27caa00e0680ea76
Ryan Day <rynday@amazon.com>
2019-03-08 08:42:39 -0500

    Add the import public key tool to the repo. Add helper functions to support the import tool and future tools.

---
+++ src/tools/io.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef AWS_CLOUDHSM_PKCS11_EXAMPLES_IO_H
+#define AWS_CLOUDHSM_PKCS11_EXAMPLES_IO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <openssl/pem.h>
+#include <openssl/err.h>
+
+size_t write_wrapped_key(char *path, char *bytes, size_t len);
+RSA *read_RSA_PUBKEY(char *path);
+
+#endif //AWS_CLOUDHSM_PKCS11_EXAMPLES_IO_H
