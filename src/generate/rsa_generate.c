eb5f7eec70f42dc6daa2ec25aa52e5604399c388
Gautham Mudambi <mudamb@amazon.com>
2020-12-06 21:32:00 -0800

    Clear ulIvBits in AES-GCM Mechanism parameters (#43)

---
+++ src/generate/rsa_generate.c
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+#include <common.h>
+
+
+/**
+ * Generate an RSA key pair suitable for signing data and verifying signatures.
+ * @param session Valid PKCS11 session.
+ * @param key_length_bits Bit size of key. Supported sizes are here: https://docs.aws.amazon.com/cloudhsm/latest/userguide/pkcs11-key-types.html
+ * @param public_key Pointer where the public key handle will be stored.
+ * @param private_key Pointer where the private key handle will be stored.
+ * @return CK_RV Value returned by the PKCS#11 library. This will indicate success or failure.
+ */
+CK_RV generate_rsa_keypair(CK_SESSION_HANDLE session,
+                           CK_ULONG key_length_bits,
+                           CK_OBJECT_HANDLE_PTR public_key,
+                           CK_OBJECT_HANDLE_PTR private_key) {
+    CK_RV rv;
+    CK_MECHANISM mech = {CKM_RSA_X9_31_KEY_PAIR_GEN, NULL, 0};
+    CK_BYTE public_exponent[] = {0x01, 0x00, 0x01};
+
+    CK_ATTRIBUTE public_key_template[] = {
+            {CKA_VERIFY,          &true_val,        sizeof(CK_BBOOL)},
+            {CKA_MODULUS_BITS,    &key_length_bits, sizeof(CK_ULONG)},
+            {CKA_TOKEN,           &false_val,       sizeof(CK_BBOOL)},
+            {CKA_PUBLIC_EXPONENT, &public_exponent, sizeof(public_exponent)},
+    };
+
+    CK_ATTRIBUTE private_key_template[] = {
+            {CKA_SIGN,  &true_val,  sizeof(CK_BBOOL)},
+            {CKA_TOKEN, &false_val, sizeof(CK_BBOOL)},
+    };
+
+    rv = funcs->C_GenerateKeyPair(session,
+                                  &mech,
+                                  public_key_template, sizeof(public_key_template) / sizeof(CK_ATTRIBUTE),
+                                  private_key_template, sizeof(private_key_template) / sizeof(CK_ATTRIBUTE),
+                                  public_key,
+                                  private_key);
+    return rv;
+}
+
+int main(int argc, char **argv) {
+    CK_RV rv;
+    CK_SESSION_HANDLE session;
+
+    struct pkcs_arguments args = {};
+    if (get_pkcs_args(argc, argv, &args) < 0) {
+        return 1;
+    }
+
+    rv = pkcs11_initialize(args.library);
+    rv = pkcs11_open_session(args.pin, &session);
+
+    CK_OBJECT_HANDLE rsa_public_key = CK_INVALID_HANDLE;
+    CK_OBJECT_HANDLE rsa_private_key = CK_INVALID_HANDLE;
+
+    rv = generate_rsa_keypair(session, 2048, &rsa_public_key, &rsa_private_key);
+    if (rv == CKR_OK) {
+        printf("RSA key generated. Public key handle: %lu, Private key handle: %lu\n", rsa_public_key, rsa_private_key);
+    } else {
+        printf("RSA key generation failed: %lu\n", rv);
+        return rv;
+    }
+    
+    pkcs11_finalize_session(session);
+    return 0;
+}
