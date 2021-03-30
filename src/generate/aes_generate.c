d1275c0c2b828a6d9ca9a471f98f70443214255c
Abhilaash <Abhilaash@users.noreply.github.com>
2021-03-30 14:55:27 -0400

    Separated key generation sample into 3 separate samples (#49)
    * Clear ulIvBits in AES-GCM Mechanism parameters (#43)
    
    * Copyright changes
    
    Authored-by: Abhilaash Velamati <velamaa@amazon.com>

---
+++ src/generate/aes_generate.c
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+#include <common.h>
+
+/**
+ * Generate an AES key.
+ * @param session  Valid PKCS#11 Session.
+ * @param key_length_bytes Byte size of key. Supported sizes are here: https://docs.aws.amazon.com/cloudhsm/latest/userguide/pkcs11-key-types.html
+ * @param key pointer to hold the resulting key handle.
+ * @return CK_RV Value returned by the PKCS#11 library. This will indicate success or failure.
+ */
+CK_RV generate_aes_key(CK_SESSION_HANDLE session,
+                       CK_ULONG key_length_bytes,
+                       CK_OBJECT_HANDLE_PTR key) {
+    CK_RV rv;
+    CK_MECHANISM mech = {CKM_AES_KEY_GEN, NULL, 0};
+
+    CK_ATTRIBUTE template[] = {
+            {CKA_SENSITIVE, &true_val,         sizeof(CK_BBOOL)},
+            {CKA_TOKEN,     &false_val,        sizeof(CK_BBOOL)},
+            {CKA_VALUE_LEN, &key_length_bytes, sizeof(CK_ULONG)}
+    };
+
+    rv = funcs->C_GenerateKey(session, &mech, template, sizeof(template) / sizeof(CK_ATTRIBUTE), key);
+    return rv;
+}
+
+int main(int argc, char **argv) {
+    CK_RV rv;
+    CK_SESSION_HANDLE session;
+
+    struct pkcs_arguments args = {};
+    if (get_pkcs_args(argc, argv, &args) < 0) {
+        return 1;
+    }
+
+    rv = pkcs11_initialize(args.library);
+    rv = pkcs11_open_session(args.pin, &session);
+
+    CK_OBJECT_HANDLE aes_key = CK_INVALID_HANDLE;
+
+    rv = generate_aes_key(session, 32, &aes_key);
+    if (CKR_OK == rv) {
+        printf("AES key generated. Key handle: %lu\n", aes_key);
+    } else {
+        printf("AES key generation failed: %lu\n", rv);
+        return rv;
+    }
+
+    pkcs11_finalize_session(session);
+    return 0;
+}
--- src/generate/ec_generate.c
+++ src/generate/ec_generate.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this
  * software and associated documentation files (the "Software"), to deal in the Software
@@ -22,64 +22,6 @@
 #include <common.h>
 
 /**
- * Generate an AES key.
- * @param session  Valid PKCS#11 Session.
- * @param key_length_bytes Byte size of key. Supported sizes are here: https://docs.aws.amazon.com/cloudhsm/latest/userguide/pkcs11-key-types.html
- * @param key pointer to hold the resulting key handle.
- * @return CK_RV Value returned by the PKCS#11 library. This will indicate success or failure.
- */
-CK_RV generate_aes_key(CK_SESSION_HANDLE session,
-                       CK_ULONG key_length_bytes,
-                       CK_OBJECT_HANDLE_PTR key) {
-    CK_RV rv;
-    CK_MECHANISM mech = {CKM_AES_KEY_GEN, NULL, 0};
-
-    CK_ATTRIBUTE template[] = {
-            {CKA_SENSITIVE, &true_val,             sizeof(CK_BBOOL)},
-            {CKA_TOKEN,     &false_val,            sizeof(CK_BBOOL)},
-            {CKA_VALUE_LEN, &key_length_bytes, sizeof(CK_ULONG)}
-    };
-
-    rv = funcs->C_GenerateKey(session, &mech, template, sizeof(template) / sizeof(CK_ATTRIBUTE), key);
-    return rv;
-}
-
-/**
- * Generate an RSA key pair suitable for signing data and verifying signatures.
- * @param session Valid PKCS11 session.
- * @param key_length_bits Bit size of key. Supported sizes are here: https://docs.aws.amazon.com/cloudhsm/latest/userguide/pkcs11-key-types.html
- * @param public_key Pointer where the public key handle will be stored.
- * @param private_key Pointer where the private key handle will be stored.
- * @return CK_RV Value returned by the PKCS#11 library. This will indicate success or failure.
- */
-CK_RV generate_rsa_keypair(CK_SESSION_HANDLE session,
-                           CK_ULONG key_length_bits,
-                           CK_OBJECT_HANDLE_PTR public_key,
-                           CK_OBJECT_HANDLE_PTR private_key) {
-    CK_RV rv;
-    CK_MECHANISM mech = {CKM_RSA_X9_31_KEY_PAIR_GEN, NULL, 0};
-    CK_BYTE public_exponent[] = {0x01, 0x00, 0x01};
-
-    CK_ATTRIBUTE public_key_template[] = {
-            {CKA_VERIFY,          &true_val,            sizeof(CK_BBOOL)},
-            {CKA_MODULUS_BITS,    &key_length_bits, sizeof(CK_ULONG)},
-            {CKA_PUBLIC_EXPONENT, &public_exponent, sizeof(public_exponent)},
-    };
-
-    CK_ATTRIBUTE private_key_template[] = {
-            {CKA_SIGN, &true_val, sizeof(CK_BBOOL)},
-    };
-
-    rv = funcs->C_GenerateKeyPair(session,
-                                  &mech,
-                                  public_key_template, sizeof(public_key_template) / sizeof(CK_ATTRIBUTE),
-                                  private_key_template, sizeof(private_key_template) / sizeof(CK_ATTRIBUTE),
-                                  public_key,
-                                  private_key);
-    return rv;
-}
-
-/**
  * Generate an EC key pair suitable for signing data and verifying signatures.
  * @param session Valid PKCS11 session.
  * @param named_curve_oid Curve to use when generating key pair. Valid curves are listed here: https://docs.aws.amazon.com/cloudhsm/latest/userguide/pkcs11-key-types.html
@@ -97,12 +39,14 @@ CK_RV generate_ec_keypair(CK_SESSION_HANDLE session,
     CK_MECHANISM mech = {CKM_EC_KEY_PAIR_GEN, NULL, 0};
 
     CK_ATTRIBUTE public_key_template[] = {
-            {CKA_VERIFY,    &true_val,           sizeof(CK_BBOOL)},
+            {CKA_VERIFY,    &true_val,       sizeof(CK_BBOOL)},
+            {CKA_TOKEN,     &false_val,      sizeof(CK_BBOOL)},
             {CKA_EC_PARAMS, named_curve_oid, named_curve_oid_len}
     };
 
     CK_ATTRIBUTE private_key_template[] = {
-            {CKA_SIGN, &true_val, sizeof(CK_BBOOL)},
+            {CKA_SIGN,  &true_val,  sizeof(CK_BBOOL)},
+            {CKA_TOKEN, &false_val, sizeof(CK_BBOOL)},
     };
 
     rv = funcs->C_GenerateKeyPair(session,
@@ -117,7 +61,7 @@ CK_RV generate_ec_keypair(CK_SESSION_HANDLE session,
 int main(int argc, char **argv) {
     CK_RV rv;
     CK_SESSION_HANDLE session;
-
+    
     struct pkcs_arguments args = {};
     if (get_pkcs_args(argc, argv, &args) < 0) {
         return 1;
@@ -125,26 +69,6 @@ int main(int argc, char **argv) {
 
     rv = pkcs11_initialize(args.library);
     rv = pkcs11_open_session(args.pin, &session);
-
-    CK_OBJECT_HANDLE aes_key = CK_INVALID_HANDLE;
-    rv = generate_aes_key(session, 32, &aes_key);
-    if (rv == CKR_OK) {
-        printf("AES key generated. Key handle: %lu\n", aes_key);
-    } else {
-        printf("AES key generation failed: %lu\n", rv);
-        return rv;
-    }
-
-    CK_OBJECT_HANDLE rsa_public_key = CK_INVALID_HANDLE;
-    CK_OBJECT_HANDLE rsa_private_key = CK_INVALID_HANDLE;
-
-    rv = generate_rsa_keypair(session, 2048, &rsa_public_key, &rsa_private_key);
-    if (rv == CKR_OK) {
-        printf("RSA key generated. Public key handle: %lu, Private key handle: %lu\n", rsa_public_key, rsa_private_key);
-    } else {
-        printf("RSA key generation failed: %lu\n", rv);
-        return rv;
-    }
 
     CK_OBJECT_HANDLE ec_public_key = CK_INVALID_HANDLE;
     CK_OBJECT_HANDLE ec_private_key = CK_INVALID_HANDLE;
@@ -160,7 +84,7 @@ int main(int argc, char **argv) {
     CK_BYTE secp384r1[] = {0x06, 0x05, 0x2b, 0x81, 0x04, 0x00, 0x22};
 
     rv = generate_ec_keypair(session, prime256v1, sizeof(prime256v1), &ec_public_key, &ec_private_key);
-    if (rv == CKR_OK) {
+    if (CKR_OK == rv) {
         printf("prime256v1 key generated. Public key handle: %lu, Private key handle: %lu\n", ec_public_key,
                ec_private_key);
     } else {
@@ -169,7 +93,7 @@ int main(int argc, char **argv) {
     }
 
     rv = generate_ec_keypair(session, secp384r1, sizeof(secp384r1), &ec_public_key, &ec_private_key);
-    if (rv == CKR_OK) {
+    if (CKR_OK == rv) {
         printf("secp384r1 key generated. Public key handle: %lu, Private key handle: %lu\n", ec_public_key,
                ec_private_key);
     } else {
@@ -178,6 +102,5 @@ int main(int argc, char **argv) {
     }
 
     pkcs11_finalize_session(session);
-
     return 0;
 }
